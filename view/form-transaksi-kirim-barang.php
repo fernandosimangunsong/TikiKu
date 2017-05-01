b2b099e42bfb7396b0171b41bdce9bc7fc731038
fernando maruli tua simangunsong <23516301@std.stei.itb.ac.id>
2017-05-01 16:53:28 +0700

    struktur direktori project

---
+++ view/form-transaksi-kirim-barang.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Form transaksi kirim barang</title>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+	<script type="text/javascript" src="js/jquery.js"></script>
+	<script type="text/javascript" src="js/bootstrap.js"></script>
+</head>
+<body>
+
+   <div class="container">		
+   <h1>Add Shimpent Data Entry</h1> 
+	<form>
+		<div class="form-group">
+			<label for="nama">Nama</label>
+			<input type="text" class="form-control">
+		</div>
+		<div class="form-group">
+			<label for="alamat">Alamat</label>
+			<input type="text" class="form-control" id="alamat">
+		</div>
+		<div class="form-group">
+			<label for="alamat">Provinsin Tujuan</label>
+			<input type="text" class="form-control" id="alamat">
+		</div>
+		
+
+		<button type="submit" class="btn btn-default">NEXT</button>
+	</form>
+   </div>
+
+</body>
+</html>
